7f986b677581e20a0030c0cfb08505d4fd6adc30
jiaowagn <wangjiaohere@gmail.com>
2017-06-25 22:00:58 +0800

    admin can adjust order state

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :ship
+        post :shipped
+        post :return
+        post :cancel
+      end
+    end
   end
 
   resources :products do
@@ -26,8 +34,9 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
-  end 
+  end
 
   resources :cart_items
 
